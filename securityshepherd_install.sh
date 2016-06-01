cb51a00f8e336ee652cfe22156b055711843bbd9
danielcuthbert <daniel@sensepost.com>
2016-06-01 11:09:35 +0100

    initial creation and push of Vagrant file and setup script

---
+++ securityshepherd_install.sh
@@ -0,0 +1,74 @@
+#! /usr/bin/env bash
+# This will install the necessary dependencies required to run the OWASP Security Shepherd
+# https://www.owasp.org/index.php/OWASP_Security_Shepherd
+# Daniel Cuthbert
+
+
+# Variables
+APPENV=local
+DBHOST=localhost
+DBNAME=dbname
+DBUSER=dbuser
+DBPASSWD=CowSaysMoo
+
+echo -e "\n--- Let's get OWASP Security Shepherd Installed... ---\n"
+
+echo -e "\n--- Updating packages list ---\n"
+apt-get -qq update
+
+echo -e "\n--- Install base packages ---\n"
+apt-get -y install vim curl build-essential python-software-properties git > /dev/null 2>&1
+
+echo -e "\n--- Updating packages list ---\n"
+apt-get -qq update
+
+echo -e "\n--- Installing MySQL specific packages and settings ---\n"
+echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+apt-get -y install mysql-server > /dev/null 2>&1
+
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
+
+echo -e "\n--- Installing Apache2  ---\n"
+apt-get -y install apache2 > /dev/null 2>&1
+
+echo -e "\n--- Setting document root to public directory ---\n"
+rm -rf /var/www
+ln -fs /vagrant/public /var/www
+
+
+echo -e "\n--- Add environment variables to Apache ---\n"
+cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
+<VirtualHost *:80>
+    DocumentRoot /var/www
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
+    SetEnv APP_ENV $APPENV
+    SetEnv DB_HOST $DBHOST
+    SetEnv DB_NAME $DBNAME
+    SetEnv DB_USER $DBUSER
+    SetEnv DB_PASS $DBPASSWD
+</VirtualHost>
+EOF
+
+echo -e "\n--- Restarting Apache ---\n"
+service apache2 restart > /dev/null 2>&1
+
+echo -e "\n--- Installing Tomcat  ---\n"
+apt-get -y install tomcat7 tomcat7-admin tomcat7-common > /dev/null 2>&1
+
+
+
+# Set envvars
+export APP_ENV=$APPENV
+export DB_HOST=$DBHOST
+export DB_NAME=$DBNAME
+export DB_USER=$DBUSER
+export DB_PASS=$DBPASSWD
